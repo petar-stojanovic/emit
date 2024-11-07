5d686c400d8871ebd51654bed86c8d77f54315a9
petar-stojanovic <banakafala1a@gmail.com>
2024-11-07 21:32:11 +0100

    added meter to show progress

--- password-checker/src/PasswordComplexity.jsx
+++ password-checker/src/PasswordComplexity.jsx
@@ -32,9 +32,31 @@ function PasswordComplexity({username, password}) {
   }, [username, password]);
 
 
+  function getProgress() {
+    return Object.values(passwordStatus).filter(it => it).length;
+  }
+
+  function getPasswordStrength() {
+    switch (getProgress()) {
+      case 5:
+        return "Very Strong";
+      case 4:
+        return "Strong";
+      case 3:
+        return "Medium";
+      case 2:
+        return "Weak";
+      default:
+        return "Very Weak";
+    }
+  }
+
   return (
     <div>
-      <h3>Password must contain:</h3>
+      <p>Password Strength: <span> {getPasswordStrength()}</span></p>
+      <meter low={3} optimum={5} max={5} high={4} value={getProgress()}/>
+
+      <p>Password must contain:</p>
       <ul className="">
         {Object.keys(passwordStatus).map((key, index) => (
           <li key={index} className={passwordStatus[key] ? "green" : "grey"}>{passwordMessages[key]}</li>)

bc7b46cfe855becdd34c4ffc2089fb13d6125343
petar-stojanovic <banakafala1a@gmail.com>
2024-11-09 16:15:29 +0100

    style header

--- hacker-news/src/components/Header.vue
+++ hacker-news/src/components/Header.vue
@@ -4,11 +4,9 @@ import {AkSettingsHorizontal} from '@kalimahapps/vue-icons';
 
 <template>
   <nav>
-    <div class="flex justify-center align-center">
-      <div class="logo">
-        <img src="@/assets/logo.png" alt="Logo"/>
-      </div>
-      <div class="logo-text">Search<br/>Hacker News</div>
+    <div class="logo">
+      <img src="@/assets/logo.png" alt="Logo"/>
+      <div class="logo-text">Search Hacker News</div>
     </div>
 
     <div class="search flex align-center justify-center">
@@ -26,18 +24,25 @@ import {AkSettingsHorizontal} from '@kalimahapps/vue-icons';
 <style scoped>
 nav {
   display: flex;
-  gap: 2rem;
+  gap: 1rem;
   justify-content: space-between;
   align-items: center;
   padding: 1rem;
   border-bottom: 1px solid #ccc;
 }
 
+.logo {
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+  justify-content: center;
+}
+
 .search {
-  gap: 1rem;
+  gap: 0.5rem;
   flex-grow: 1;
   width: 100%;
-  padding: 0.51rem;
+  padding: 0.5rem;
 }
 
 .search input {
@@ -57,8 +62,7 @@ nav {
 .logo-text {
   color: gray;
   font-weight: 500;
-  font-size: 1.25rem;
-  line-height: 1.5rem;
-  margin: 0 1rem
+  font-size: 1rem;
+  line-height: 1.15rem;
 }
 </style>

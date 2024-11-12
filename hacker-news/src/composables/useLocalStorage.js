777298dbf0d57229c0a5336d8ff636be85bd0839
petar-stojanovic <banakafala1a@gmail.com>
2024-11-12 13:46:17 +0100

    added cache expiration date

--- hacker-news/src/composables/useLocalStorage.js
+++ hacker-news/src/composables/useLocalStorage.js
@@ -1,16 +1,25 @@
 import {ref, watch} from "vue";
 
-export default function useLocalStorage(initialValue, key) {
+export default function useLocalStorage(initialValue, key, expireTime = 1000 * 60 * 60 * 24) {
   const val = ref(initialValue);
 
   const storageVal = localStorage.getItem(key);
 
   if (storageVal) {
-    val.value = JSON.parse(storageVal);
+    const {expireTime, ...parsedVal} = JSON.parse(storageVal);
+    if (expireTime < new Date().getTime()) {
+      localStorage.removeItem(key);
+    } else {
+      val.value = parsedVal;
+    }
   }
 
   watch(val, val => {
-      localStorage.setItem(key, JSON.stringify(val));
+      const objWithExpireTime = {
+        ...val,
+        expireTime: new Date().getTime() + expireTime
+      }
+      localStorage.setItem(key, JSON.stringify(objWithExpireTime));
     },
     {deep: true});
 
